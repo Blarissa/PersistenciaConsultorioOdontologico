7c4e11f19f97f5306df3bc2ba0569e67605afc72
Blarissa <larissabrasil009@gmail.com>
2022-11-14 16:16:49 -0300

    Implementando classe vertice

--- ExercicioRevisao1/ExercicioRevisao1/Program.cs
+++ ExercicioRevisao1/ExercicioRevisao1/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+
+            /*
+             * Questão 1
             Piramide piramide = new(4);
             Console.WriteLine("Piramide tamanho 4");
             piramide.Desenha();
@@ -11,8 +14,24 @@
             piramide = new(6);
             Console.WriteLine("Piramide tamanho 6");
             piramide.Desenha();
+            */
 
+            /*
+             * Questão 2*/
 
+            Vertice v1, v2;
+
+            v1 = new Vertice(1.8,4);
+            v2 = new Vertice(2.5,1);
+
+            Console.WriteLine("Distancia de entre ("+ v1.X + ","+ v1.Y +") e " +
+                "("+ v2.X + ", "+ v2.Y +"): "+ v1.Distancia(v2));
+            
+            //movendo vertice 2
+            v2 = v2.Move(v1.X, v1.Y);
+
+            Console.WriteLine("Distancia de entre (" + v1.X + "," + v1.Y + ") e " +
+                "(" + v2.X + ", " + v2.Y + "): " + v1.Distancia(v2));
         }
     }
 }
---
+++ ExercicioRevisao1/ExercicioRevisao1/Vertice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioRevisao1
+{
+    internal class Vertice
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Vertice() { }
+
+        public Vertice(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public double Distancia(Vertice v1)
+        {
+            if(Diferente(v1))
+            {
+                double dist = Math.Pow((v1.X - X),2) + Math.Pow((v1.Y - Y), 2);
+                return Math.Sqrt(dist);
+            }
+            return 0;
+            
+        }
+
+        public Vertice Move(double x, double y)
+        {
+            return new Vertice(x,y);
+        }
+
+        public bool Diferente(Vertice v1)
+        {
+            if (X != v1.X || Y != v1.X)
+                return true;
+            return false;
+        }
+
+    }
+}
