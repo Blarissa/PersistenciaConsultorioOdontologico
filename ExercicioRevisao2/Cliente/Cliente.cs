abb9023c6c2c3e6875c1750575efa207f0140de0
Blarissa <larissabrasil009@gmail.com>
2022-11-23 12:23:32 -0300

    Implementando projeto Validacliente

---
+++ ExercicioRevisao2/Cliente/Cliente.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValidaCliente
+{
+    internal class Cliente
+    {
+        private string nome;
+        private long cpf;
+        private DateTime dataNascimento;
+        private float renda;
+        private char estadoCivil;
+        private int? dependentes;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                if (value.Length >= 5)
+                    nome = value;
+                else
+                    throw new ArgumentException("\nNome precisa ter pelo menos 5 caracteres!");
+            }
+
+        }
+        public long Cpf
+        {
+            get
+            {
+                return cpf;
+            }
+            set
+            {
+
+                int tamanho = value.ToString().Length;
+
+                if (tamanho == 11 || tamanho == 10)
+                    cpf = value;
+                else
+                    throw new ArgumentException("\nCPF com tamanho inválido!");
+            }
+
+        }
+        public DateTime DataNascimento
+        {
+            get
+            {
+                return dataNascimento;
+            }
+            set
+            {
+                int dias = DateTime.Now.Subtract(dataNascimento).Days;
+
+                if (dias / 365 >= 18)
+                    dataNascimento = value;
+                else
+                    throw new ArgumentException("\nMenor de 18 anos!");
+            }
+        }
+        public float Renda
+        {
+            get
+            {
+                return renda;
+            }
+            set
+            {
+                //retorna true se tiver virgula no numero
+                bool virgula = value.ToString().Contains(",");
+                //retorna true se tiver 2 casas decimais
+                bool casas = value.ToString().Split(",").Length == 2;
+
+                if (virgula && casas)
+                    renda = value;
+                else
+                    throw new ArgumentException("\nValor diferente do exigido!");
+            }
+        }
+        public char EstadoCivil
+        {
+            get
+            {
+                return estadoCivil;
+            }
+            set
+            {
+                string valores = "CSVD";
+
+                if (valores.Contains(value))
+                    estadoCivil = value;
+                else
+                    throw new ArgumentException("\nEstado civil inválido!");
+
+            }
+        }
+        public int? Dependentes
+        {
+            get
+            {
+                return dependentes;
+            }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    dependentes = value;
+                else
+                    throw new ArgumentException("\nNumero de dependentes inválidos!");
+            }
+        }
+
+        public Cliente() { }
+
+        public Cliente(string nome, long cpf, DateTime dataNascimento,
+            float renda, char estadoCivil, int? dependentes)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.dataNascimento = dataNascimento;
+            this.renda = renda;
+            this.estadoCivil = estadoCivil;
+            this.dependentes = dependentes;
+
+        }
+
+        public override string ToString()
+        {
+            string imprime = "\n------------Cliente------------" +
+                 "\nNome: " + Nome
+               + "\nCPF: " + Cpf
+               + "\nData de nascimento: " + DataNascimento.ToString("dd/MM/yyyy")
+               + "\nRenda Mensal: " + Renda.ToString("C", NumberFormatInfo.CurrentInfo)
+               + "\nEstado Civil: " + EstadoCivil
+               + "\nNumero de Dependente: " + Dependentes
+               + "\n";
+
+            return imprime;
+        }
+    }
+}
---
+++ ExercicioRevisao2/Cliente/ValidaCliente.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace ValidaCliente
+{
+    internal class ValidaCliente
+    {
+        public Cliente Cliente { get; set; }
+
+        public bool Nome(string nome)
+        {
+            return nome.Length >= 5;
+        }
+
+        public bool CPF(string cpf)
+        {
+            if (cpf.Length == 11)
+            {
+                if (cpf.All(c=> cpf[0] == c))
+                    return false;
+ 
+                int soma = 0;
+                int i = 0;
+                for (int j = 10; j >= 2; j--, i++)        
+                    soma += (int)Char.GetNumericValue(cpf[i]) * j;
+                
+                int resto = soma % 11;
+                
+                bool J = false;
+                bool K = false;
+
+                if (resto == 0 || resto == 1)
+                    J = (int) Char.GetNumericValue(cpf[9]) == 0;
+                else if (resto >= 2 && resto <= 10)
+                    J = (int) Char.GetNumericValue(cpf[9]) == (11 - resto);
+
+                soma = 0;
+                i = 0;
+
+                for (int j = 11; j >= 2 ; j--,i++)
+                    soma += (int)Char.GetNumericValue(cpf[i]) * j;
+
+                resto = soma % 11;
+
+                if (resto == 0 || resto == 1)
+                    K = (int) Char.GetNumericValue(cpf[10]) == 0;
+                else if (resto >= 2 && resto <= 10)
+                    K = (int)Char.GetNumericValue(cpf[10]) == (11 - resto);
+
+                return J && K;
+            }         
+
+            return false;
+        }
+        public bool DataDeNascimento(string dataNascimento)
+        {
+            DateTime data = DateTime.Parse(dataNascimento);
+
+            return (DateTime.Now.Subtract(data).Days / 365) >= 18;
+        }        
+        public bool Renda(String renda)
+        {   
+            float valor = float.Parse(renda);
+            bool formato = valor.ToString("F").Equals(renda);         
+               
+            return (valor > 0 && formato);
+        }
+        public bool EstadoCivil(string estadoCivil)
+        {
+            char[] estado = {'C','S','V','D'};
+
+            return estado.Contains(char.Parse(estadoCivil));
+        }
+        public bool Dependentes(string dependentes)
+        {
+            int valor = int.Parse(dependentes);
+            return valor >= 0 && valor <= 10;
+
+        }
+    }
+}
