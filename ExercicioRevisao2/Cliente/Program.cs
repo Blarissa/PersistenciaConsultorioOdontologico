5f5caf8b531e87e8307ad1260ded3a2d0ae52476
Blarissa <larissabrasil009@gmail.com>
2022-11-23 12:23:32 -0300

    Implementando projeto Validacliente

---
+++ ExercicioRevisao2/Cliente/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Globalization;
+
+namespace Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            ValidaCliente v = new ValidaCliente();
+
+
+        }
+    }
+}
