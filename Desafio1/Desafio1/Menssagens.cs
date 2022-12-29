1ebb08bae20670674895c2b1d7e98772d9ecc0d8
Blarissa <larissabrasil009@gmail.com>
2022-12-28 21:24:14 -0300

    Implementando outras classes no projeto

---
+++ Desafio1/Desafio1/Menssagens.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Desafio1
+{
+    internal class Menssagens
+    {
+        //Mensagens de erro
+        //opção
+        public static string opcaoInvalida { get => "Erro: opção inválida!"; }
+        //CPF
+        public static string CpfIvalido { get => "Erro: CPF inválido!"; }
+        public static string CpfExistente { get => "Erro: CPF já cadastrado!"; }
+        //Nome
+        public static string NomeIvalido { get => "Erro: nome deve ter pelo menos 5 caracteres!"; }
+        //Data
+        public static string DtInvalida { get => "Erro: data inválida!"; }
+        public static string DtInvalidaFormato { get => "Erro: data deve ser fornecida no formato DD/MM/AAAA!"; }
+        public static string DtConsultaInvalida { get => "Erro: agendamento deve ser para um período futuro!"; }
+        public static string DtInicialInvalida { get => "Erro: data inicial deve ser maior que a data atual!"; }
+        public static string DtFinalInvalida { get => "Erro: data final deve ser maior que a data inicial!"; }
+        //Hora
+        public static string HrInvalida { get => "Erro: hora deve ser fornecida no formato HHMM!"; }
+        public static string HrInvalidaFuncionamento { get => "Erro: horário de atendimento é das 8:00h às 19:00h!"; }
+        public static string HrFinalInvalida { get => "Erro: hora final deve ser maior que a hora inicial!"; }
+        //Consulta
+        public static string ConsultExistente { get => "Erro: já existe uma consulta agendada nesse horário!"; }
+        //Agendamento
+        public static string AgendInexistente { get => "Erro: agendamento não encontrado!"; }
+        //Paciente
+        public static string IdadeInvalida { get => "Erro: paciente deve ter pelo menos 13 anos!"; }
+        public static string PacienteInixistente { get => "Erro: paciente não cadastrado!"; }
+        public static string PacienteAgendado { get => "Erro: paciente está agendado!"; }
+
+        //Mensagens de sucesso
+        public static string PacienteCadastrado { get => "Paciente cadastrado com sucesso!"; }
+        public static string PacienteExcluido { get => "Paciente excluído com sucesso!"; }
+        public static string AgendamentoRealizado { get => "Agendamento realizado com sucesso!"; }
+        public static string AgendamentoCancelado { get => "Agendamento cancelado com sucesso!"; }
+
+    }
+}
---
+++ Desafio1/Desafio1/PacienteController.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Desafio1
+{
+    internal class PacienteController
+    {
+        List<Paciente> Pacientes = new();
+        
+        //Adionando paciente
+        public void Adiciona() {
+            long CPF = EntradaDeDados.LerCPF();
+            if(PacienteExiste(CPF)){
+                Console.WriteLine(Menssagens.CpfExistente);
+                CPF = EntradaDeDados.LerCPF();
+            }
+
+            string nome = EntradaDeDados.LerNome();
+            DateTime dtNasc = EntradaDeDados.LerDtNascimento();            
+
+            Pacientes.Add(new(CPF, nome, dtNasc));
+            Console.WriteLine(Menssagens.PacienteCadastrado);
+        }       
+        
+        //Retorna o paciente de determinado CPF 
+        public Paciente? PesquisaCPF(long CPF)
+        {   
+            if(PacienteExiste(CPF))           
+                return Pacientes.Find(paciente => paciente.CPF.Equals(CPF));
+
+            return null;                       
+        }
+
+        //Retorna se o paciente existe
+        public bool PacienteExiste(long CPF)
+        {
+            return Pacientes.Exists(paciente => paciente.CPF.Equals(CPF));
+        }        
+
+        //Removendo paciente
+        public void Remove()
+        {
+            long CPF = EntradaDeDados.LerCPF();
+            //Se não encontrar o paciente na lista imprime a mensagem de erro
+            while (!PacienteExiste(CPF)){
+                Console.WriteLine(Menssagens.PacienteInixistente);
+                CPF = EntradaDeDados.LerCPF();
+            }
+
+            Pacientes.Remove(PesquisaCPF(CPF));
+            Console.WriteLine(Menssagens.PacienteExcluido);          
+        }       
+        
+        public override string? ToString()
+        {
+            string str = ("").PadRight(60, '-') + "\n"
+                       + $"{"CPF",-11} {"Nome",-33} {"Dt.Nasc."} {"Idade"}\n"
+                       + ("").PadRight(60, '-') + "\n";
+
+            Pacientes.ForEach(p =>
+                str += $"{p.CPF,-11} " +
+                       $"{p.Nome,-33} " +
+                       $"{p.DtNascimento.ToShortDateString()} " +
+                       $"{p.Idade}\n");
+                //+$"{"",-12}{str1}\n"
+                //+ $"{"",-12}{str2}\n");
+
+            return str;
+        }
+
+        //Listar pacientes(ordenado por nome)
+        public string? PacientesOrdemNome()
+        {            
+            Pacientes = Pacientes.OrderBy(p => p.Nome).ToList();                
+
+            return ToString();
+        }
+
+        //Listar pacientes(ordenado por CPF)
+        public string? PacientesOrdemCPF()
+        {
+            Pacientes = Pacientes.OrderBy(p => p.CPF).ToList();
+            return ToString();
+        }
+        */
+    }
+}
