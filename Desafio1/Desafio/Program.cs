c501aad15e3ac467f544e129a536623d2b020ae8
Blarissa <larissabrasil009@gmail.com>
2023-01-03 16:41:56 -0300

    Ajeitando projeto

---
+++ Desafio1/Desafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Globalization;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Menu.Principal() >= 0){
+                continue;            
+            }
+        }
+    }
+}
