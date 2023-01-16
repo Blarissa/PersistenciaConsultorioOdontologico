6f94778b62e3b58b96cd3438762b8d9a62068cb9
Blarissa <larissabrasil009@gmail.com>
2023-01-16 05:12:59 -0300

    Inserindo documentação da classe Program

--- Desafio2/ConversorMonetario/Program.cs
+++ Desafio2/ConversorMonetario/Program.cs
@@ -1,34 +1,51 @@
 ﻿using ConversorMonetario;
 using System.Net.Http.Json;
 
-using HttpClient cliente = new();
-
-await ConverteMoeda(cliente);
-
-static async Task<List<string>> SimbolosPermitidos(HttpClient cliente)
+internal class Program
 {
-    var uri = "https://api.exchangerate.host/symbols";    
+    private static async Task Main(string[] args)
+    {
+        using HttpClient cliente = new();
 
-    var permitidos = await cliente.GetFromJsonAsync<Simbolos>(uri);
-
-    return permitidos.Permitidos;
-}
-
-static async Task ConverteMoeda(HttpClient cliente)
-{
-    var simbolosPermitidos = await SimbolosPermitidos(cliente);
-    
-    var uri = Controller.URIParaConversao(simbolosPermitidos);
-
-    HttpResponseMessage response = await cliente.GetAsync(uri);
-    Controller.Status(response);
-
-    if (!response.IsSuccessStatusCode) 
         await ConverteMoeda(cliente);
 
-    Console.WriteLine("\nConvertendo...\n");
-    Resultado resultado = await cliente.GetFromJsonAsync<Resultado>(uri);
-  
-    Controller.MostrarResultados(resultado);
-    await ConverteMoeda(cliente);
+        ///<summary>
+        /// Define a lista de <see cref="Simbolos.Permitidos"/>.
+        ///</summary>
+        ///<param name="cliente">Representa o valor do <see cref="cliente"/>.</param>
+        ///<returns>
+        ///Retorna a lista de <see cref="Simbolos.Permitidos"/>.
+        ///</returns>
+        static async Task<List<string>> SimbolosPermitidos(HttpClient cliente)
+        {
+            var uri = "https://api.exchangerate.host/symbols";
+
+            var permitidos = await cliente.GetFromJsonAsync<Simbolos>(uri);
+
+            return permitidos.Permitidos;
+        }
+
+        ///<summary>
+        /// Define a conversão monetária.
+        ///</summary>
+        ///<param name="cliente">Representa o valor do <see cref="cliente"/>.</param>       
+        static async Task ConverteMoeda(HttpClient cliente)
+        {
+            var simbolosPermitidos = await SimbolosPermitidos(cliente);
+
+            var uri = Controller.URIParaConversao(simbolosPermitidos);
+
+            HttpResponseMessage response = await cliente.GetAsync(uri);
+            Controller.Status(response);
+
+            if (!response.IsSuccessStatusCode)
+                await ConverteMoeda(cliente);
+
+            Console.WriteLine("\nConvertendo...\n");
+            Resultado resultado = await cliente.GetFromJsonAsync<Resultado>(uri);
+
+            Controller.MostrarResultados(resultado);
+            await ConverteMoeda(cliente);
+        }
+    }
 }
