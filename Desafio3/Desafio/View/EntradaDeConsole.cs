e986973ec71ea56a6d2c877ace9377cbbdde3e92
Blarissa <larissabrasil009@gmail.com>
2023-02-15 12:01:31 -0300

    chore: Movendo classe para a camsada correta

--- Desafio3/Desafio/View/EntradaDeConsole.cs
+++ Desafio3/Desafio/View/EntradaDeConsole.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Desafio.Data.DadosComConsole
+namespace Desafio.View
 {
     internal class EntradaDeConsole : IRecebimentoDeDados
     {
