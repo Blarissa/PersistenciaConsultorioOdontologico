41a5eec155a85432b27fc6df7ea314dfc33b9665
Larissa Brasil <54214827+Blarissa@users.noreply.github.com>
2023-04-13 01:34:32 -0300

    refactor: criando configurações do banco de dados

---
+++ Desafio/Data/Configuracoes/ConsultaConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Desafio.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Data.Configuracoes
+{
+    public class ConsultaConfiguration : IEntityTypeConfiguration<Consulta>
+    {
+        public void Configure(EntityTypeBuilder<Consulta> builder)
+        {
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.DataHoraInicial)
+                .HasColumnType("Timestamp without Time Zone")
+                .IsRequired();
+
+            builder.Property(c => c.DataHoraFinal)
+                .HasColumnType("Timestamp without Time Zone")
+                .IsRequired();
+            
+            var sql = "AGE(data_hora_final, data_hora_inicial)";
+
+            builder.Property(c => c.TempoConsulta)
+                .HasColumnType("Interval")                
+                .HasComputedColumnSql(sql, true);
+
+            builder.HasOne(c => c.Paciente)
+                   .WithMany(p => p.Consultas)
+                   .HasForeignKey(c => c.CPFPaciente);
+        }
+    }
+}
---
+++ Desafio/Data/Configuracoes/PacienteConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Desafio.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Data.Configuracoes
+{
+    public class PacienteConfiguration : IEntityTypeConfiguration<Paciente>
+    {
+        public void Configure(EntityTypeBuilder<Paciente> builder)
+        {
+            builder.Property(p => p.CPF)
+                .ValueGeneratedNever();
+            
+            builder.HasKey(p => p.CPF);
+
+            builder.Property(p => p.Nome)
+                .IsRequired();
+
+            builder.Property(p => p.DataDeNascimento)
+                .HasColumnType("Date")
+                .IsRequired();                                  
+        }
+    }
+}
